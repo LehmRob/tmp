c689fb8f0dc6c72c41532eea0a96d6036d8cf643
Robert Lehmann <lehmrob@gmail.com>
2017-02-11 10:03:15 +0100

    Initial commit

---
+++ rust/cli/src/main.rs
@@ -0,0 +1,35 @@
+use std::env;
+
+fn print_help() {
+    println!("Usage:
+--help: Showing the help
+--version: Showing the version");
+}
+
+fn show_version() {
+    println!("v0.1.0");
+}
+
+fn main() {
+    println!("hello_friend");
+    let args: Vec<String> = env::args().collect();
+
+    match args.len() {
+        1 => {
+            println!("Not enough arguments. Try --help");
+        },
+        2 => {
+            let arg = &args[1];
+            if arg == "--help" {
+                print_help();
+            } else if arg == "--version" {
+                show_version();
+            } else {
+                println!("Unknown argument"
+            }
+        },
+        _ => {
+            print_help();
+        }
+    }
+}
