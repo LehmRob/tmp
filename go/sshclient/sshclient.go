d08ca45a0300d5b9c2da88f670b0abe972027e5c
Robert Lehmann <lehmrob@gmail.com>
2017-05-13 22:13:07 +0200

    update

--- go/sshclient/sshclient.go
+++ go/sshclient/sshclient.go
@@ -1,65 +1,66 @@
 package main
 
 import (
-    "golang.org/x/crypto/ssh"
-    "io"
+	"io"
 	"os"
+
+	"golang.org/x/crypto/ssh"
 )
 
 func main() {
-    sshConfig := &ssh.ClientConfig{
-        User: "vmpadmin",
-        Auth: []ssh.AuthMethod{
-            ssh.Password("JtP8mQ9E"),
-        },
-    }
+	sshConfig := &ssh.ClientConfig{
+		User: "user",
+		Auth: []ssh.AuthMethod{
+			ssh.Password("secret"),
+		},
+	}
 
-    conn, err := ssh.Dial("tcp", "vmp-clan.de:22", sshConfig)
-    if err != nil {
-        panic(err.Error())
-    }
-    defer conn.Close()
+	conn, err := ssh.Dial("tcp", "example.org:22", sshConfig)
+	if err != nil {
+		panic(err.Error())
+	}
+	defer conn.Close()
 
-    session, err := conn.NewSession()
-    if err != nil {
-        panic(err.Error())
-    }
-    defer session.Close()
+	session, err := conn.NewSession()
+	if err != nil {
+		panic(err.Error())
+	}
+	defer session.Close()
 
-    modes := ssh.TerminalModes{
-    	ssh.ECHO:          0,     // disable echoing
-    	ssh.TTY_OP_ISPEED: 14400, // input speed = 14.4kbaud
-    	ssh.TTY_OP_OSPEED: 14400, // output speed = 14.4kbaud
-    }
+	modes := ssh.TerminalModes{
+		ssh.ECHO:          0,     // disable echoing
+		ssh.TTY_OP_ISPEED: 14400, // input speed = 14.4kbaud
+		ssh.TTY_OP_OSPEED: 14400, // output speed = 14.4kbaud
+	}
 
-    if err := session.RequestPty("xterm", 80, 40, modes); err != nil {
-    	session.Close()
-        panic(err.Error())
-    }
+	if err := session.RequestPty("xterm", 80, 40, modes); err != nil {
+		session.Close()
+		panic(err.Error())
+	}
 
-    stdin, err := session.StdinPipe()
-    if err != nil {
-        panic(err.Error())
-    }
+	stdin, err := session.StdinPipe()
+	if err != nil {
+		panic(err.Error())
+	}
 
-    go io.Copy(stdin, os.Stdin)
+	go io.Copy(stdin, os.Stdin)
 
-    stdout, err := session.StdoutPipe()
-    if err != nil {
-        panic(err.Error())
-    }
+	stdout, err := session.StdoutPipe()
+	if err != nil {
+		panic(err.Error())
+	}
 
-    go io.Copy(os.Stdout, stdout)
+	go io.Copy(os.Stdout, stdout)
 
-    stderr, err := session.StderrPipe()
-    if err != nil {
-        panic(err.Error())
-    }
+	stderr, err := session.StderrPipe()
+	if err != nil {
+		panic(err.Error())
+	}
 
-    go io.Copy(os.Stderr, stderr)
+	go io.Copy(os.Stderr, stderr)
 
-    err = session.Run("uptime")
-    if err != nil {
-        panic(err.Error())
-    }
+	err = session.Run("uptime")
+	if err != nil {
+		panic(err.Error())
+	}
 }
