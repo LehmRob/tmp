2e49f154c8a6f80924dbda4c5321c65146ef1125
Robert Lehmann <lehmrob@gmail.com>
2020-01-04 12:53:37 +0100

    Remove js directory

--- js/websocket_example/server.js
+++
@@ -1,7 +0,0 @@
-express = require('express');
-app = express();
-server = require('http').createServer(app);
-io = require('socket.io').listen(server)
-conf = require('./config.json')
-
-server.listen(conf.port)
