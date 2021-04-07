64c6d4eedc4d8c461816f3d028f7bdaef1233f0a
Robert Lehmann <lehmrob@gmail.com>
2021-04-07 22:09:14 +0200

    Update rust example

--- rust/csvparser/src/main.rs
+++ rust/csvparser/src/main.rs
@@ -2,25 +2,48 @@ use std::io::BufReader;
 use std::io::BufRead;
 use std::fs::File;
 use std::env;
+use std::process;
 
-fn main() {
-    println!("Hello, world!");
+fn print_help(name: &String) {
+    println!("Usage {}:
+    --help: Show help", name);
+}
 
-    let args: Vec<String> = env::args().collect();
-    if args.len() <= 1 {
-        panic!("Not enought arguments");
-    }
-
-    let filename = &args[1];
-    println!("This is the file {}", filename);
-
+fn analyze(filename: &str) {
     let file = File::open(filename).unwrap();
     let file_reader = BufReader::new(&file);
 
     let mut counter: u32 = 1;
-    for line in file_reader.lines() {
-        let l = line.unwrap();
-        println!("Line {}, {}", counter, l);
+    for _ in file_reader.lines() {
         counter += 1;
     }
+
+    println!("Counted {} in {} lines", counter, filename);
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    match args.len() {
+        1 => {
+            println!("Not enough arguments. Try --help");
+            process::exit(0);
+        },
+        2 => {
+            match args[1].as_str() {
+                "--help" => {
+                    print_help(&args[0]);
+                    process::exit(0);
+                },
+                path => {
+                    analyze(path);
+                }
+            }
+        },
+        _ => {
+            println!("Not enough arguments. Try --help");
+            process::exit(0);
+        }
+    }
+
 }
--- rust/tuple/src/main.rs
+++ rust/tuple/src/main.rs
@@ -2,8 +2,8 @@ fn main() {
     println!("Hello, friend!");
 
     // Declare 
-    let t1 = (1, 2);
-    
+    let t1 = ("t", '\t');
+
     // Indexing
     println!("First element {}", t1.0);
     println!("Second element {}", t1.1);
