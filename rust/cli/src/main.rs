cd3f90b7a27a39db3d1a5a177b94fb9dec3b3316
Robert Lehmann <lehmrob@gmail.com>
2022-01-16 20:26:14 +0100

    Improve the cli example a little bit

--- rust/cli/src/main.rs
+++ rust/cli/src/main.rs
@@ -1,9 +1,13 @@
 use std::env;
+use std::process;
+use std::string::String;
 
 fn print_help() {
     println!("Usage:
 --help: Showing the help
---version: Showing the version");
+--version: Showing the version
+--name=<NAME>: Printing name"
+);
 }
 
 fn show_version() {
@@ -14,22 +18,26 @@ fn main() {
     println!("hello_friend");
     let args: Vec<String> = env::args().collect();
 
-    match args.len() {
-        1 => {
-            println!("Not enough arguments. Try --help");
-        },
-        2 => {
-            let arg = &args[1];
-            if arg == "--help" {
-                print_help();
-            } else if arg == "--version" {
-                show_version();
-            } else {
-                println!("Unknown argument");
-            }
-        },
-        _ => {
-            print_help();
+    if args.len() <= 1 {
+        println!("Not enough arguments. Try --help");
+        process::exit(1);
+    }
+
+    let arg = &args[1];
+    if arg == "--help" || arg == "-h" {
+        print_help();
+        process::exit(0);
+    } else if arg == "--version" {
+        show_version();
+    } else if arg.contains("--name") {
+        let parts:Vec<&str> = arg.split("=").collect();
+        if parts.len() < 2 {
+            println!("Invalid arguments");
+            process::exit(1);
         }
+        let name = parts[1];
+        println!("We do the name {}", name);
+    } else {
+        println!("Unknown argument");
     }
 }
